d394be1fcc1b13cd03eba09ac3d71fc1b8d4641c
AgiTochka <agatochka567@gmail.com>
2022-08-03 18:08:12 +0300

    feat: create userbar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './header.css';
 import iconMenu from './img/menu.svg';
+import iconSearch from './img/search.svg';
 
 const Header = () => {
     const Pannel = () => {
@@ -12,20 +13,24 @@ const Header = () => {
         header.classList.toggle('animHeader');
         workspace.classList.toggle('animWorkspace');
     }
-        return (
-            <div className='main-header' id='header'>
-                <div className='menu'>
-                    <button onClick={()=>Pannel()}> <img src={iconMenu} alt={''} /></button>
-                    <h2>ToDo</h2>
-                </div>
+    return (
+        <div className='main-header' id='header'>
+            <div className='menu'>
+                <button onClick={() => Pannel()}> <img src={iconMenu} alt={''} /></button>
+                <h2>ToDo</h2>
+            </div>
+            <div className='rightSide'>
                 <div className='userInfo'>
                     <label>Username</label>
                 </div>
                 <div className='search'>
                     <input placeholder='Search Project' />
+                    <img className='btnSearch' src={iconSearch} ait=''/>
                 </div>
-
             </div>
-        );
+
+
+        </div>
+    );
 }
 export default Header;
