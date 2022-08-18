65ae95b238799d7cde317c0c95abe95a432db5ba
AgiTochka <agatochka567@gmail.com>
2022-08-18 13:58:01 +0000

    feat: create content in dashboard page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,18 +3,20 @@ import './Home.css';
 import Navigation from '../../components/navigation/navigation';
 import Header from "../../components/header/header";
 
-
-
-
-
 const Home = () => {
-
-
+    const CurrentDate = () =>{
+        const current = new Date();
+        const monthName = current.toLocaleString("default", {month: "short"});
+        const dayName = current.toLocaleDateString("default", { weekday: 'short' });
+        return dayName + ' ' + current.getDate() + ' ' + monthName + ', ' + current.getFullYear();
+    }
     return (
         <div className='main-home'>
             <Navigation />
             <Header />
             <div className='workspace' id="workspace">
+                <h2>Today</h2>
+                <p>{CurrentDate()}</p>
             </div>
           
         </div>
